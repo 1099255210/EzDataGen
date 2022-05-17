073fa595561a3477d03090ac410183458c61148e
1099255210 <1099255210@qq.com>
2022-05-18 00:37:13 +0800

    add vue.js, add some more functions.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var btnDownload = document.getElementById('download');
 var btnClear = document.getElementById('clear');
+var btnPlus = document.getElementById('plus');
+var btnMinus = document.getElementById('minus');
+var btnReset = document.getElementById('reset');
+var link = document.getElementById('dl');
+link.style.visibility = "hidden";
+
 var pointset = [];
 var filename = 'output.txt';
 
-var xneg = 5;
-var xpos = 10;
-var yneg = 5;
 var ypos = 10;
+var yneg = 0;
+var xneg = 0;
+var xpos = 10;
+var scaleX = xpos - xneg;
+var scaleY = ypos - yneg;
 
 // function init() {
 //   canvas.width = window.innerWidth;
@@ -22,19 +30,88 @@ canvas.addEventListener('mousemove', draw, false);
 canvas.addEventListener('mousedown', addPoint, false);
 btnDownload.addEventListener('click', download, false);
 btnClear.addEventListener('click', clearCanvas, false);
+btnPlus.addEventListener('click', zoomIn, false);
+btnMinus.addEventListener('click', zoomOut, false);
+btnReset.addEventListener('click', resetCanvas, false);
 
 function clearCanvas() {
-  document.location.reload();
+  pointset = [];
+  link.style.visibility = "hidden";
+}
+
+function resetCanvas() {
+  pointset = [];
+  ypos = 10;
+  yneg = 0;
+  xneg = 0;
+  xpos = 10;
+  scaleX = xpos - xneg;
+  scaleY = ypos - yneg;
+  link.style.visibility = "hidden";
+  info.updateInfo();
 }
 
 function addPoint(e) {
-  var scaleX = xpos - xneg;
-  var scaleY = ypos - yneg;
   var curserX = xneg + e.offsetX * scaleX / canvas.width;
   var curserY = ypos - e.offsetY * scaleY / canvas.height;
   pointset.push([curserX, curserY]);
   console.log([curserX, curserY]);
 }
+
+function zoomIn() {
+  ypos = ypos - scaleY / 4;
+  yneg = yneg + scaleY / 4;
+  xneg = xneg + scaleX / 4;
+  xpos = xpos - scaleX / 4;
+  scaleX = xpos - xneg;
+  scaleY = ypos - yneg;
+}
+
+function zoomOut() {
+  ypos = ypos + scaleY / 2;
+  yneg = yneg - scaleY / 2;
+  xneg = xneg - scaleX / 2;
+  xpos = xpos + scaleX / 2;
+  scaleX = xpos - xneg;
+  scaleY = ypos - yneg;
+}
+
+// Vue part
+
+var info = new Vue({
+  el: '#info',
+  data: {
+    ypos: ypos,
+    yneg: yneg,
+    xneg: xneg,
+    xpos: xpos,
+  },
+  methods: {
+    zoomIn: function() {
+      zoomIn();
+      this.updateInfo();
+    },
+    zoomOut: function() {
+      zoomOut();
+      this.updateInfo();
+    },
+    updateInfo: function() {
+      this.ypos = ypos;
+      this.yneg = yneg;
+      this.xneg = xneg;
+      this.xpos = xpos;
+    },
+    updateAxis: function() {
+      ypos = parseInt(this.ypos);
+      yneg = parseInt(this.yneg);
+      xneg = parseInt(this.xneg);
+      xpos = parseInt(this.xpos);
+      scaleX = xpos - xneg;
+      scaleY = ypos - yneg;
+    }
+  }
+});
+
 
 // Draw functions
 
@@ -44,6 +121,7 @@ function draw(e) {
   drawBg();
   drawCurser(e);
   drawSavedPoints();
+  drawPointer();
 }
 
 function drawBg() {
@@ -63,8 +141,6 @@ function drawSavedPoints() {
     var realPos = pointset[i];
     var rX = realPos[0];
     var rY = realPos[1];
-    var scaleX = xpos - xneg;
-    var scaleY = ypos - yneg;
     var oX = (rX - xneg) * canvas.width / scaleX;
     var oY = (ypos - rY) * canvas.height / scaleY;
     drawPoint([oX, oY]);
@@ -80,7 +156,7 @@ function drawPoint(position) {
   ctx.closePath();
 }
 
-// Output part.
+// Output part
 
 function download() {
   var dataStr = '';
@@ -90,11 +166,8 @@ function download() {
 
   console.log(dataStr);
 
-  // var element = document.createElement('a');
-  // element.setAttribute('href', "data:text/plain;charset=utf-8" + encodeURIComponent(dataStr));
-  // element.setAttribute('download', filename);
-
-  // document.body.appendChild(element);
-  // element.click();
-  // document.body.removeChild(element);
+  var blob = new Blob([dataStr]);
+  link.style.visibility = "visible";
+  link.download = filename;
+  link.href = URL.createObjectURL(blob);
 }
